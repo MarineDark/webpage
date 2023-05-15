13f2f08553c1ea98d0982227fbf26c3897b58706
Hamza Mughal <hamza@prodesquare.com>
2023-05-16 04:28:22 +0500

    click to copy color code

--- src/components/Color.vue
+++ src/components/Color.vue
@@ -2,10 +2,14 @@
 defineProps({
   color: String,
 });
+
+const copyColorCode = (code) => {
+  navigator.clipboard.writeText(code);
+};
 </script>
 
 <template>
-  <button>
+  <button @click="copyColorCode(color)">
     <span :style="`background: ${color}`"></span>
     <span>{{ color }}</span>
   </button>
