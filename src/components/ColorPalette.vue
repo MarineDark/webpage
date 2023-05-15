69c93a5b96c7f7ced6a2be65d573f484ac8af7d4
Hamza Mughal <hamza@prodesquare.com>
2023-05-16 03:12:05 +0500

    using color palette store

--- src/components/ColorPalette.vue
+++ src/components/ColorPalette.vue
@@ -1,5 +1,13 @@
+<script setup>
+import { useColorsStore } from "../stores/colors";
+
+const colors = useColorsStore().palette;
+</script>
+
 <template>
   <section class="color-palette">
     <h2>Color Palette</h2>
+
+    {{ colors }}
   </section>
 </template>
