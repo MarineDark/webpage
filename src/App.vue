07f5053c2874f9993b051d42324c7da04573a1d7
Hamza Mughal <hamza@prodesquare.com>
2023-05-16 02:51:53 +0500

    basic color palette container ready

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import TheHeader from "./components/TheHeader.vue";
 import Screenshot from "./components/Screenshot.vue";
+import ColorPalette from "./components/ColorPalette.vue";
 </script>
 
 <template>
   <main class="container">
     <TheHeader />
     <Screenshot />
+    <ColorPalette />
   </main>
 </template>
---
+++ src/components/ColorPalette.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="color-palette">
+    <h2>Color Palette</h2>
+  </section>
+</template>
