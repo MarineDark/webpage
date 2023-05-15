9c665af38694524f1f83d83e809a047f4e1eb589
Hamza Mughal <hamza@prodesquare.com>
2023-05-16 03:10:04 +0500

    color palette store

---
+++ src/stores/colors.js
@@ -0,0 +1,24 @@
+import { defineStore } from "pinia";
+
+export const useColorsStore = defineStore("colors", {
+  state: () => ({
+    palette: [
+      "#002221",
+      "#ea3431",
+      "#00b6b6",
+      "#f8b017",
+      "#4894fd",
+      "#e01dca",
+      "#1ab2ad",
+      "#99dddb",
+      "#006562",
+      "#ea3431",
+      "#00b6b6",
+      "#f8b017",
+      "#4894fd",
+      "#e01dca",
+      "#1ab2ad",
+      "#e6f6f6",
+    ],
+  }),
+});
