e4e580aa44264e3c7ee05c19875a926c8c30e171
Hamza Mughal <hamza@prodesquare.com>
2023-05-17 02:17:53 +0500

    showing links on footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { useLinksStore } from "../stores/links";
+
+const links = useLinksStore().links;
+</script>
+
 <template>
-  <footer>&copy; {{ new Date().getFullYear() }} Marine Dark</footer>
+  <footer>
+    <ul>
+      <li v-for="link in links">
+        <a :href="link.url">{{ link.title }}</a>
+      </li>
+    </ul>
+  </footer>
 </template>
