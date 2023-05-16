ce67fedb05d459bef2d3e25aa0cd0b3388f4c4f0
Hamza Mughal <hamza@prodesquare.com>
2023-05-17 03:56:29 +0500

    app store

---
+++ src/stores/app.js
@@ -0,0 +1,9 @@
+import { defineStore } from "pinia";
+
+export const useAppStore = defineStore("app", {
+  state: () => ({
+    title: "Marine Dark",
+    year_created: "2023",
+    about: "Colorscheme inspired by deep marine hues, designed by ProDeSquare",
+  }),
+});
