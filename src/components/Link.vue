3352e0b9c241cbb6e06372c360d6db10dc469cc8
Hamza Mughal <hamza@prodesquare.com>
2023-05-17 03:51:13 +0500

    links in the footer

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,9 +1,14 @@
 <script setup>
+import LinkIcon from "./LinkIcon.vue";
+
 defineProps({
   link: Object,
 });
 </script>
 
 <template>
-  <a :href="link.url">{{ link.title }}</a>
+  <a :href="link.url">
+    <span>{{ link.title }}</span>
+    <LinkIcon />
+  </a>
 </template>
