f764d73187ee62dfa150717b64b097e74ddf80eb
Hamza Mughal <hamza@prodesquare.com>
2023-05-17 03:38:18 +0500

    Link icon component

---
+++ src/components/LinkIcon.vue
@@ -0,0 +1,52 @@
+<template>
+  <svg
+    version="1.1"
+    viewBox="0 0 128 128"
+    xml:space="preserve"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+  >
+    <g>
+      <g>
+        <line
+          x1="42.288"
+          x2="105.999"
+          y1="22"
+          y2="22.001"
+          style="
+            fill: none;
+            stroke: currentcolor;
+            stroke-width: 12;
+            stroke-linecap: square;
+            stroke-miterlimit: 10;
+          "
+        ></line>
+        <line
+          x1="105.999"
+          x2="106.001"
+          y1="22.001"
+          y2="85.713"
+          style="
+            fill: none;
+            stroke: currentcolor;
+            stroke-width: 12;
+            stroke-linecap: square;
+            stroke-miterlimit: 10;
+          "
+        ></line>
+      </g>
+      <line
+        x1="105.999"
+        x2="15.999"
+        y1="22.001"
+        y2="112"
+        style="
+          fill: none;
+          stroke: currentcolor;
+          stroke-width: 12;
+          stroke-miterlimit: 10;
+        "
+      ></line>
+    </g>
+  </svg>
+</template>
