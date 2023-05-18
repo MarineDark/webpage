a220f15f98ce248b2295f1c45900b992901807e2
Hamza Mughal <hamza@prodesquare.com>
2023-05-19 03:04:04 +0500

    st and xresources link

--- src/stores/links.js
+++ src/stores/links.js
@@ -11,6 +11,14 @@ export const useLinksStore = defineStore("links", {
         title: "Alacritty",
         url: "https://github.com/MarineDark/marine-dark-terminals/blob/master/alacritty/marine-dark.yaml",
       },
+      {
+        title: "ST Patch",
+        url: "https://github.com/MarineDark/marine-dark-terminals/blob/master/st/st-marine-dark-0.9.diff",
+      },
+      {
+        title: "Xresources",
+        url: "https://github.com/MarineDark/marine-dark-terminals/blob/master/xresources/Xresources",
+      },
     ],
   }),
 });
